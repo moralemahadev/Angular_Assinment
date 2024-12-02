af2dc431b512e5c306aa91e2b5b9631143c750b3
moralemahadev <moralemahadev5@gmail.com>
2024-12-03 02:43:20 +0530

    Add files via upload

---
+++ Assinment_11/Application_3/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Assinment_11_3';
+}
